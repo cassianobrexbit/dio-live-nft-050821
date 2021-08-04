6d4924c467c8693f3f407ff6182a62bb590f1bf8
Cassiano Ricardo de Oliveira Peres <37804181+cassianobrexbit@users.noreply.github.com>
2021-08-04 09:21:49 -0300

    Create dioNFT.sol

---
+++ sources/contracts/dioNFT.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.0;
+ 
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+ 
+contract DIONFT is NFTokenMetadata, Ownable { 
+ 
+  constructor() {
+    nftName = "DIO NFT";
+    nftSymbol = "DIO";
+  }
+ 
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+  }
+ 
+}
